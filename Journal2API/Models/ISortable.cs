0e5236dcd7056466e339eca80fe6b626645a8d8c
Mizhal <mizhal@gmail.com>
2015-09-08 23:13:08 +0200

    Implementación de isortable sin probar

--- Journal2API/Models/ISortable.cs
+++ Journal2API/Models/ISortable.cs
@@ -12,22 +12,72 @@ namespace Journal2API.Models
 
     public static class ISortableExtensions
     {
-        public static void InsertBefore<T>(this ISortable<T> item, ISortable<T> before_this) where T : ISortable<T>
+        public static void InsertBefore<T>(this ISortable<T> item, ISortable<T> before_this) where T : class, ISortable<T>
         {
+            using (var ctx = new JournalContext())
+            {
+                int before_pos = before_this.Position;
+                item.Position = before_pos;
 
+                before_this.Position++;
+                var set = ctx.Set<T>();
+                set.Add(item as T);
+                set.Add(before_this as T);
+                var next_items = set.Where(x => before_this.Position >= item.Position);
+                foreach (var it in next_items)
+                {
+                    it.Position++;
+                    set.Add(it as T);
+                }
+
+                ctx.SaveChanges();
+            }
         }
 
-        public static void InsertAfter<T>(this ISortable<T> item, ISortable<T> after_this) where T : ISortable<T>
+        public static void InsertAfter<T>(this ISortable<T> item, ISortable<T> after_this) where T : class, ISortable<T>
         {
+            using (var ctx = new JournalContext())
+            {
+                int after_pos = after_this.Position;
+                item.Position = after_pos + 1;
 
+                after_this.Position++;
+                var set = ctx.Set<T>();
+                set.Add(item as T);
+                set.Add(after_this as T);
+                var next_items = set.Where(x => x.Position >= item.Position);
+                foreach(var it in next_items)
+                {
+                    it.Position++;
+                    set.Add(it as T);
+                }
+
+                ctx.SaveChanges();
+            }
+        }
+
+        public static void SwapWith<T>(this ISortable<T> item, ISortable<T> other) where T : class, ISortable<T>
+        {
+            using (var ctx = new JournalContext())
+            {
+                int pos_this = item.Position;
+                int pos_other = other.Position;
+                other.Position = pos_this;
+                item.Position = pos_other;
+
+                var set = ctx.Set<T>();
+                set.Add(other as T);
+                set.Add(item as T);
+                ctx.SaveChanges();
+            }
         }
 
         public static T Previous<T>(this ISortable<T> item) where T : class, ISortable<T>
         {
             using(var ctx = new JournalContext())
             {
-                var found = ctx.Set<T>().OrderBy(r => (r as T).Position).
-                    First(r => (r as T).Position < item.Position);
+                var found = ctx.Set<T>().OrderBy(r => r.Position).
+                    First(r => r.Position < item.Position);
                 return found;
             }
         }
@@ -36,8 +86,8 @@ namespace Journal2API.Models
         {
             using (var ctx = new JournalContext())
             {
-                var found = ctx.Set<T>().OrderBy(r => (r as T).Position).
-                    First(r => (r as T).Position > item.Position);
+                var found = ctx.Set<T>().OrderBy(r => r.Position).
+                    First(r => r.Position > item.Position);
                 return found;
             }
         }

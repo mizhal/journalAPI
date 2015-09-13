d129ae3741ced9f333b394a94c054d7450c125c6
Mizhal <mizhal@gmail.com>
2015-09-13 13:31:35 +0200

    Mejor organización de ISortable, mejora de rendimiento y contemplar casos extremos. Sin probar.

--- Journal2API/Models/ISortable.cs
+++ Journal2API/Models/ISortable.cs
@@ -16,19 +16,14 @@ namespace Journal2API.Models
         {
             using (var ctx = new JournalContext())
             {
-                int before_pos = before_this.Position;
-                item.Position = before_pos;
+                ISortable<T> prev_element = before_this.Previous();
 
-                before_this.Position++;
+                int position;
+                SortableHelper.InsertBetween(prev_element, before_this, out position);
+                item.Position = position;
+
                 var set = ctx.Set<T>();
                 set.Add(item as T);
-                set.Add(before_this as T);
-                var next_items = set.Where(x => x.Position >= before_this.Position);
-                foreach (var it in next_items)
-                {
-                    it.Position++;
-                    set.Add(it as T);
-                }
 
                 ctx.SaveChanges();
             }
@@ -38,19 +33,14 @@ namespace Journal2API.Models
         {
             using (var ctx = new JournalContext())
             {
-                int after_pos = after_this.Position;
-                item.Position = after_pos + 1;
+                ISortable<T> next_element = after_this.Next();
+            
+                int position;
+                SortableHelper.InsertBetween(after_this, next_element, out position);
+                item.Position = position;
 
-                after_this.Position++;
                 var set = ctx.Set<T>();
                 set.Add(item as T);
-                set.Add(after_this as T);
-                var next_items = set.Where(x => x.Position >= item.Position);
-                foreach(var it in next_items)
-                {
-                    it.Position++;
-                    set.Add(it as T);
-                }
 
                 ctx.SaveChanges();
             }
@@ -90,6 +80,89 @@ namespace Journal2API.Models
                     First(r => r.Position > item.Position);
                 return found;
             }
+        }
+
+        public static bool IsFirst<T>(this ISortable<T> item) where T : class, ISortable<T>
+        {
+            return item.Previous() == null;
+        }
+
+        public static bool IsLast<T>(this ISortable<T> item) where T : class, ISortable<T>
+        {
+            return item.Next() == null;
+        }
+
+        static class SortableHelper
+        {
+            public const int GAP = 100;
+
+            public static void InsertBetween<T>(ISortable<T> first, ISortable<T> second, out int position) where T : class, ISortable<T>
+            {
+                int second_position;
+                int first_position;
+
+                if(first == null)
+                {
+                    first_position = 0;
+                    second_position = second.Position;
+                } else if (second == null)
+                {
+                    first_position = first.Position;
+                    second_position = first_position + GAP;
+                } else
+                {
+                    first_position = first.Position;
+                    second_position = second.Position;
+                }
+
+                if(second_position >= int.MaxValue) throw new WrapAlert();
+
+                if (second_position - first_position >= 2)
+                {
+                    position = first_position + (second_position - first_position) / 2;
+                } else
+                {
+                    RearrangePositions<T>();
+                    InsertBetween(first, second, out position);
+                }
+            }
+
+            public static void RearrangePositions<T>() where T : class, ISortable<T>
+            {
+                // Re-allocates positions evenly separated to allow insertions between elements
+                // without the necessity to recalculate the positions of next elements 
+
+                // eventualy (when a gap is exhausted) this methods should be called with a cost of O(n)
+                // but this is an amortized cost over all insertion costs we saved with this solution
+
+                using (var ctx = new JournalContext())
+                {
+                    int counter = 1; // it's recommendable to put a gap before the first element too to enable front insertions
+                    var set = ctx.Set<T>();
+
+                    if (set.Count() * GAP >= int.MaxValue)
+                    {
+                        throw new WrapAlert();
+                    }
+
+                    foreach (var element in set)
+                    {
+                        element.Position = counter * GAP;
+                        counter++;
+                        set.Add(element);
+                    }
+                    ctx.SaveChanges();
+                }
+            }
+        }
+
+    }
+
+    public class WrapAlert : Exception
+    {
+        public WrapAlert() : base("Wrap alert")
+        {
+
         }
     }
 }
